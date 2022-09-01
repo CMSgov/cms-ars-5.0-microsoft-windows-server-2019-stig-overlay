234bcf88ca0a9f29c29217c3688a00c3cbdee460
karikarshivani <karikarshivani@gmail.com>
2022-09-01 09:41:30 -0400

    Rearranged controls
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -7,23 +7,12 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
     tag nist: ["SC-45 (1)"]
   end
 
-  control 'V-93571' do
-    tag nist: ["CM-6 b", "SC-7 (5)"]
-  end
-
-  control "V-93567" do
-    title "Windows Server 2019 must employ automated mechanisms to determine the state of system components with regard to flaw remediation every 72 hours."
-    desc  "Without the use of automated mechanisms to scan for security flaws on a continuous and/or periodic basis, the operating system or other system components may remain vulnerable to the exploits presented by undetected software flaws."
-    desc  "check", "Verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding."
-    desc  "fix", "Configure the operating system to employ automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours."
-
-    describe "A manual review is required to verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding." do	
-      skip "A manual review is required to verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding."	
-    end
-  end
-
   control "V-93457" do
     tag nist: ["AC-2 (3)"]
+  end
+
+  control 'V-93571' do
+    tag nist: ["CM-6 b", "SC-7 (5)"]
   end
 
   ## SEMANTIC
@@ -51,6 +40,17 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
     desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since the US DoD CCEB is not applicable to CMS"
     describe "Not applicable for this CMS ARS 5.0 overlay, since the US DoD CCEB is not applicable to CMS" do 
       skip "Not applicable for this CMS ARS 5.0 overlay, since the US DoD CCEB is not applicable to CMS"
+    end
+  end
+
+  control "V-93567" do
+    title "Windows Server 2019 must employ automated mechanisms to determine the state of system components with regard to flaw remediation every 72 hours."
+    desc  "Without the use of automated mechanisms to scan for security flaws on a continuous and/or periodic basis, the operating system or other system components may remain vulnerable to the exploits presented by undetected software flaws."
+    desc  "check", "Verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding."
+    desc  "fix", "Configure the operating system to employ automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours."
+
+    describe "A manual review is required to verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding." do	
+      skip "A manual review is required to verify the operating system employs automated mechanisms to determine the state of system components with regard to flaw remediation no less often than once every seventy-two (72) hours. If it does not, this is a finding."	
     end
   end
 
